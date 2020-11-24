59e0a24cef589c2b262a2bdb92c0121bc4a30fb0
Kevin <grj@bwg.art>
2020-11-24 11:27:30 +0800

    4，2020-11-24

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -19,29 +19,61 @@
       };
     },
     mounted() {
-      let arr = [];
-      for (let i = 0; i <= this.rows; i++) {
-        let now_arr = [];
-        for (let j = 0; j < this.rows - i; j++) {
-          now_arr[j] = '-';
-        }
-        for (let m = 0; m < 2 * i + 1; m++) {
-          now_arr.push('*');
-        }
-        arr.push(now_arr);
+      // 老的浏览器可能根本没有实现 mediaDevices，所以我们可以先设置一个空的对象
+      if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
       }
-      for (let i = 0; i < this.rows; i++) {
-        let now_arr = [];
-        for (let j = 0; j <= i; j++) {
-          now_arr[j] = '-';
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+          // 首先，如果有getUserMedia的话，就获得它
+          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+          console.log(getUserMedia);
+          // 一些浏览器根本没实现它 - 那么就返回一个error到promise的reject来保持一个统一的接口
+          if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+
+          // 否则，为老的navigator.getUserMedia方法包裹一个Promise
+          return new Promise(function (resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
         }
-        for (let m = 0; m < 2 * (this.rows - i) - 1; m++) {
-          now_arr.push('*');
-        }
-        arr.push(now_arr);
       }
-      console.log(arr);
-      this.arr = arr;
+      navigator.mediaDevices.getUserMedia().then(res => {
+        console.log(res);
+      }).catch(err=>{
+        console.log(err);
+      })
+
+
+    },
+    methods: {
+      test() {
+        let arr = [];
+        for (let i = 0; i <= this.rows; i++) {
+          let now_arr = [];
+          for (let j = 0; j < this.rows - i; j++) {
+            now_arr[j] = '-';
+          }
+          for (let m = 0; m < 2 * i + 1; m++) {
+            now_arr.push('*');
+          }
+          arr.push(now_arr);
+        }
+        for (let i = 0; i < this.rows; i++) {
+          let now_arr = [];
+          for (let j = 0; j <= i; j++) {
+            now_arr[j] = '-';
+          }
+          for (let m = 0; m < 2 * (this.rows - i) - 1; m++) {
+            now_arr.push('*');
+          }
+          arr.push(now_arr);
+        }
+        console.log(arr);
+        this.arr = arr;
+      }
     }
   };
 </script>
